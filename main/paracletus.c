9861986dead527fda530210d618fbeb1fcf91b1a
ephiguxta <fabiogustavo@tuta.io>
2025-02-10 10:42:16 -0400

    função "uart_data_income" para pegar os dados e atribuir em um buffer

--- main/paracletus.c
+++ main/paracletus.c
@@ -3,6 +3,7 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
+#include "string.h"
 
 #define RXD_PORT 5
 #define TXD_PORT 4
@@ -28,10 +29,12 @@ typedef struct {
 } gps_t;
 
 static void nmea_parser_start_gps_uart(void);
+static void uart_data_income(void *);
 
 void app_main(void)
 {
 	nmea_parser_start_gps_uart();
+	xTaskCreate(uart_data_income, "uart_data_income", 4096, NULL, 10, NULL);
 }
 
 static void nmea_parser_start_gps_uart(void)
@@ -48,4 +51,25 @@ static void nmea_parser_start_gps_uart(void)
 	ESP_ERROR_CHECK(uart_driver_install(0, 256, 0, 0, NULL, 0));
 	ESP_ERROR_CHECK(uart_param_config(0, &uart_config));
 	ESP_ERROR_CHECK(uart_set_pin(0, TXD_PORT, RXD_PORT, -1, -1));
+}
+
+static void uart_data_income(void *arg) {
+	size_t data_length = 0;
+
+	uint8_t *data = (uint8_t *) malloc(128);
+
+	while(1) {
+		uart_get_buffered_data_len(0, (size_t *) &data_length);
+
+		if(data_length >= 10) {
+			uint8_t len = uart_read_bytes(0, (char *) data, data_length, 100);
+
+			if(len) {
+				printf("(%s)\n", (char *) data);
+				memset(data, '\0', 128);
+			}
+		}
+
+		vTaskDelay(50);
+	}
 }
