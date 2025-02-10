e598455bc24bdb1cd415673e36895cccd603da27
ephiguxta <fabiogustavo@tuta.io>
2025-02-10 15:06:52 -0400

    tratando o dado recebido pelo UART e verificando se é uma sentença que contém abertura e fechamento da NMEA

--- main/paracletus.c
+++ main/paracletus.c
@@ -28,8 +28,12 @@ typedef struct {
 	gps_time_t time;
 } gps_t;
 
+// UART
 static void nmea_parser_start_gps_uart(void);
 static void uart_data_income(void *);
+
+// GPS
+esp_err_t get_nmea_sentence(const char *buffer, char *nmea_sentence);
 
 void app_main(void)
 {
@@ -57,6 +61,7 @@ static void uart_data_income(void *arg) {
 	size_t data_length = 0;
 
 	uint8_t *data = (uint8_t *) malloc(128);
+	char gps_sentence[128] = { 0 };
 
 	while(1) {
 		uart_get_buffered_data_len(0, (size_t *) &data_length);
@@ -66,11 +71,61 @@ static void uart_data_income(void *arg) {
 			uint8_t len = uart_read_bytes(0, (char *) data, data_length, 100);
 
 			if(len) {
-				printf("(%s)\n", (char *) data);
+				esp_err_t ret = get_nmea_sentence((const char *) data, gps_sentence);
+				if(ret == ESP_OK) {
+					printf("(%s)\n", gps_sentence);
+				}
+
 				memset(data, '\0', 128);
 			}
 		}
 
 		vTaskDelay(50);
 	}
+}
+
+uint8_t digit_pos(const char *buffer, char target) {
+	// função de suporte pra retornar a posição de um dígito
+	// passado no segundo parâmetro
+	//
+
+	uint8_t sign_pos = 0;
+
+	for(uint8_t i = 0; i < 128; i++) {
+		if(buffer[i] == target) {
+			sign_pos = i;
+			break;
+		}
+	}
+
+	return sign_pos;
+}
+
+void fill_buffer(const char *buffer, char *nmea_sentence, uint8_t begin, uint8_t end) {
+	for(uint8_t i = begin; i <= (end + 2); i++) {
+		nmea_sentence[i - begin] = buffer[i];
+	}
+
+	// pula os dois dígitos de checksum e insere '\0'
+	nmea_sentence[end + 3] = '\0';
+}
+
+esp_err_t get_nmea_sentence(const char *buffer, char *nmea_sentence) {
+
+	// procura pelo cifrão
+	uint8_t dollar_sign_pos = 0;
+	dollar_sign_pos = digit_pos(buffer, '$');
+
+	// procura pelo asterístico
+	uint8_t asterisk_pos = 0;
+	asterisk_pos = digit_pos(buffer, '*');
+
+	// se na sentença não tiver dados ou delimitador de início/fim, ignora a linha
+	if(asterisk_pos <= dollar_sign_pos) {
+		return ESP_FAIL;
+	}
+
+	fill_buffer(buffer, nmea_sentence, dollar_sign_pos, asterisk_pos);
+
+	return ESP_OK;
 }
